49de7185edef97a53733197d42cb80054ceda7f9
marshyski <marshyski@gmail.com>
2015-04-10 01:01:12 -0400

    Redisgned plural to post to elasticsearch instead of client API

--- src/main.go
+++ src/main.go
@@ -1,36 +1,74 @@
 package main
 
 import (
-    "log"
     "fmt"
     "net/http"
+    "net"
+    "time"
+    "os"
+    "io"
     "os/exec"
     "io/ioutil"
+    "strings"
+    "bytes"
+    "github.com/spf13/viper"
     "github.com/shirou/gopsutil/mem"
     "github.com/shirou/gopsutil/disk"
-    "github.com/shirou/gopsutil/cpu"
     "github.com/shirou/gopsutil/host"
     "github.com/shirou/gopsutil/load"
-    "github.com/shirou/gopsutil/net"
 )
 
-func all(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-       errorHandler(w, r, http.StatusNotFound)
-       return
+// HTTP client timeout
+var timeout = time.Duration(300 * time.Millisecond)
+
+func dialTimeout(network, addr string) (net.Conn, error) {
+        return net.DialTimeout(network, addr, timeout)
+}
+
+func main() {
+
+  for {
+
+    // Configuration file settings using key-value
+    viper.SetConfigName("plural")
+    viper.AddConfigPath("/opt/plural/conf")
+    err := viper.ReadInConfig()
+    if err != nil {
+       fmt.Println("No Configuration File Using DEFAULTS")
     }
 
+    // Default settings if no config file is supplied
+    viper.SetDefault("elastic_host", "localhost")
+    viper.SetDefault("elastic_port", "9200")
+    viper.SetDefault("environment", "dev")
+
+    elastic_host := viper.GetString("elastic_host")
+    elastic_port := viper.GetString("elastic_port")
+    environment := viper.GetString("environment")
+
+    transport := http.Transport{
+       Dial: dialTimeout,
+    }
+
+    client := http.Client{
+       Transport: &transport,
+    }
+
+    // UNIX system commands
     kernelver := exec.Command("uname","-r")
     kernelverout, err := kernelver.Output()
-    timezone  := exec.Command("date","+%Z")
+    kernelverstring := string(kernelverout)
+    timezone := exec.Command("date","+%Z")
     timezoneout, err := timezone.Output()
+    timezonestring := string(timezoneout)
 
     hostname := exec.Command("hostname","-f")
-    hostcut := exec.Command("cud","-d.","-f","2-")
-    hostnameOut, _ := hostname.StdoutPipe()
+    hostcut := exec.Command("cut","-d.","-f","2-")
+    hostnameOut, err := hostname.StdoutPipe()
     hostname.Start()
     hostcut.Stdin = hostnameOut
-    domainname, _ := hostcut.Output()
+    domainname, err := hostcut.Output()
+    domainstring := string(domainname)
 
     if err != nil {
        fmt.Println(err.Error())
@@ -39,77 +77,141 @@ func all(w http.ResponseWriter, r *http.Request) {
 
     v, _ := mem.VirtualMemory()
     k, _ := disk.DiskUsage("/")
-    c, _ := cpu.CPUInfo()
     h, _ := host.HostInfo()
     l, _ := load.LoadAvg()
-    n, _ := net.NetInterfaces()
 
-   // AWS 
+    // ElasticSearch endpoint
+    elastic_url := "http://" + elastic_host + ":" + elastic_port + "/" + environment + "/" + h.Hostname
 
-   response, err := http.Get("http://169.254.169.254/latest")
-   if response.Status == string("200 OK") {
-      if err != nil {
-         fmt.Printf("%s", err)
-       } else {
-          amiid, err := http.Get("http://169.254.169.254/latest/meta-data/ami-id")
-          defer amiid.Body.Close()
-          amiidOut, err := ioutil.ReadAll(amiid.Body)
-          instanceid, err := http.Get("http://169.254.169.254/latest/meta-data/instance-id")
-          defer instanceid.Body.Close()
-          instanceidOut, err := ioutil.ReadAll(instanceid.Body)
-          instancetype, err := http.Get("http://169.254.169.254/latest/meta-data/instance-type")
-          defer instancetype.Body.Close()
-          instancetypeOut, err := ioutil.ReadAll(instancetype.Body)
-          availabilityzone, err := http.Get("http://169.254.169.254/latest/meta-data/placement/availability-zone")
-          defer availabilityzone.Body.Close()
-          availabilityzoneOut, err := ioutil.ReadAll(availabilityzone.Body)
-          securitygroups, err := http.Get("http://169.254.169.254/latest/meta-data/security-groups")
-          defer securitygroups.Body.Close()
-          securitygroupsOut, err := ioutil.ReadAll(securitygroups.Body)
-          profile, err := http.Get("http://169.254.169.254/latest/meta-data/profile")
-          defer profile.Body.Close()
-          profileOut, err := ioutil.ReadAll(profile.Body)
-          if err != nil {
-             fmt.Printf("%s", err)
-          }
-          fmt.Fprintf(w, "ec2_ami_id: %s\n", string(amiidOut))
-          fmt.Fprintf(w, "ec2_instance_id: %s\n", string(instanceidOut))
-          fmt.Fprintf(w, "ec2_instance_type: %s\n", string(instancetypeOut))
-          fmt.Fprintf(w, "ec2_availability_zone: %s\n", string(availabilityzoneOut))
-          fmt.Fprintf(w, "ec2_security_groups: %s\n", string(securitygroupsOut))
-          fmt.Fprintf(w, "ec2_profile: %s\n", string(profileOut))
-      }
+    // JSON file name
+    filename := h.Hostname + ".json"
+
+    // Create JSON file
+    f, err := os.Create(filename)
+    if err != nil {
+       fmt.Println(err.Error())
+       return
+    }
+    n, err := io.WriteString(f, "{")
+    if err != nil {
+       fmt.Println(n, err)
+       return
     }
 
-    fmt.Fprintf(w, "memorytotal: %v\nmemoryfree: %v\nmemoryused: %f%%\n", v.Total, v.Free, v.UsedPercent)
-    fmt.Fprintf(w, "disktotal: %v\ndiskfree: %v\ndiskused: %f%%\n", k.Total, k.Free, k.UsedPercent)
-    fmt.Fprintf(w, "cpu: %v\n", c)
-    fmt.Fprintf(w, "hostname: %v\nuptime: %v\nos: %v\nplatform: %v\nplatformfamily: %v\nplatformverison: %v\nvirtualizationsystem: %v\nvirtualizationrole: %v\n", h.Hostname, h.Uptime, h.OS, h.Platform, h.PlatformFamily, h.PlatformVersion, h.VirtualizationSystem, h.VirtualizationRole)
-    fmt.Fprintf(w, "load1: %v\nload5: %v\nload15: %v\n", l.Load1, l.Load5, l.Load15)
-    fmt.Fprintf(w, "networkinterfaces: %v\n", n)
-    fmt.Fprintf(w, "kernelversion: %s", string(kernelverout))
-    fmt.Fprintf(w, "timezone: %s", string(timezoneout))
-    fmt.Fprintf(w, "domain: %s", domainname)
-}
+    top := `
+    "diskfree": "%v",
+    "disktotal": "%v",
+    "diskused": "%v",
+    "domain": "%s",`
 
-func Log(handler http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-       log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-       handler.ServeHTTP(w, r)
-    })
-}
-
-func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
-    w.WriteHeader(status)
-    if status == http.StatusNotFound {
-       fmt.Fprint(w, "404")
+    topLine := fmt.Sprintf(top, k.Free, k.Total, k.UsedPercent, strings.TrimSpace(domainstring))
+    writeTop, err := io.WriteString(f, topLine)
+    if err != nil {
+       fmt.Println(writeTop, err)
+       return
     }
-}
 
-func main() {
-     http.HandleFunc("/", all)
-     err := http.ListenAndServe(":8000", Log(http.DefaultServeMux))
-     if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-     }
+    // Local meta-data AWS
+    awsResponse, err := client.Get("http://169.254.169.254/latest")
+    if awsResponse != nil {
+       amiid, err := http.Get("http://169.254.169.254/latest/meta-data/ami-id")
+       defer amiid.Body.Close()
+       amiidOut, err := ioutil.ReadAll(amiid.Body)
+       instanceid, err := http.Get("http://169.254.169.254/latest/meta-data/instance-id")
+       defer instanceid.Body.Close()
+       instanceidOut, err := ioutil.ReadAll(instanceid.Body)
+       instancetype, err := http.Get("http://169.254.169.254/latest/meta-data/instance-type")
+       defer instancetype.Body.Close()
+       instancetypeOut, err := ioutil.ReadAll(instancetype.Body)
+       availabilityzone, err := http.Get("http://169.254.169.254/latest/meta-data/placement/availability-zone")
+       defer availabilityzone.Body.Close()
+       availabilityzoneOut, err := ioutil.ReadAll(availabilityzone.Body)
+       securitygroups, err := http.Get("http://169.254.169.254/latest/meta-data/security-groups")
+       defer securitygroups.Body.Close()
+       securitygroupsOut, err := ioutil.ReadAll(securitygroups.Body)
+       profile, err := http.Get("http://169.254.169.254/latest/meta-data/profile")
+       defer profile.Body.Close()
+       profileOut, err := ioutil.ReadAll(profile.Body)
+       if err != nil {
+          fmt.Println(err.Error())
+          return
+       }
+
+       aws := `
+    "ec2_ami_id": "%s",
+    "ec2_availability_zone": "%s",
+    "ec2_instance_id": "%s",
+    "ec2_instance_type": "%s",
+    "ec2_profile": "%s",
+    "ec2_security_groups": "%s",`
+
+       awsLine := fmt.Sprintf(aws, amiidOut, availabilityzoneOut, instanceidOut, instancetypeOut, profileOut, securitygroupsOut)
+       writeAWS, err := io.WriteString(f, awsLine)
+       if err != nil {
+          fmt.Println(writeAWS, err)
+          return
+       }
+    }
+
+    bottom := `
+    "hostname": "%s",
+    "kernelversion": "%s",
+    "load15": "%v",
+    "load1": "%v",
+    "load5": "%v",
+    "memoryfree": "%v",
+    "memorytotal": "%v",
+    "memoryused": "%v",
+    "os": "%v",
+    "platform": "%v",
+    "platformfamily": "%v",
+    "platformverison": "%v",
+    "timezone": "%s",
+    "uptime": "%v",
+    "virtualizationrole": "%v",
+    "virtualizationsystem": "%v"
+  }`
+
+    bottomLine := fmt.Sprintf(bottom, h.Hostname, strings.TrimSpace(kernelverstring), l.Load15, l.Load1, l.Load5, v.Free, v.Total, v.UsedPercent, h.OS, h.Platform, h.PlatformFamily, h. PlatformVersion, strings.TrimSpace(timezonestring), h.Uptime, h.VirtualizationRole, h.VirtualizationSystem)
+    writeLine, err := io.WriteString(f, bottomLine)
+    if err != nil {
+       fmt.Println(writeLine, err)
+       return
+    }
+
+    f.Close()
+
+    // Check to see if ElasticSearch server is up
+    elasticResponse, err := client.Get(elastic_url)
+    if elasticResponse != nil {
+       jsonStr,err := ioutil.ReadFile(filename)
+       if err != nil {
+          fmt.Println(err.Error())
+          return
+       }
+       fmt.Println("ElasticSearch EndPoint:", elastic_url)
+       reqDelete, err := http.NewRequest("DELETE", elastic_url, nil)
+       respDelete, err := http.DefaultClient.Do(reqDelete)
+       fmt.Println("Delete ElasticSearch Type Status:", respDelete.Status)
+       reqPost, err := http.NewRequest("POST", elastic_url, bytes.NewBuffer(jsonStr))
+       reqPost.Header.Set("Content-Type", "application/json")
+
+       clientReq := &http.Client{}
+       respPost, err := clientReq.Do(reqPost)
+       if err != nil {
+          fmt.Println(err.Error())
+       }
+       defer respPost.Body.Close()
+
+       fmt.Println("POST JSON ElasticSearch Type Status:", respPost.Status)
+       postBody, _ := ioutil.ReadAll(respPost.Body)
+       fmt.Println("POST Response Body:", string(postBody))
+    } else {
+       fmt.Println("Unable to Connect to ElasticSearch Server:", "http://" + elastic_host + ":" + elastic_port)
+    }
+
+    // Sleep time for, for loop
+    time.Sleep(5 * time.Minute)
+
+  }
 }
